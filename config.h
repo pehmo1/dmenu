69a83c4f4f906d6f5abff78045a240a22a530431
Siim B <m2ngija@gmail.com>
2021-01-10 18:27:11 +0200

    lineheight patch, doesn't work maybe

--- config.h
+++ config.h
@@ -15,7 +15,10 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 500;
 
 /*
  * Characters not considered part of a word while deleting words
